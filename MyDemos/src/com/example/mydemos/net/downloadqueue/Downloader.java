2ab00ebce7647ce098a12590ef67fec7f9758068
xwd2000 <xwd2000@gmail.com>
2015-08-20 14:09:22 +0800

    deleted:    MyDemos/src/com/example/mydemos/net/downloadqueue/CommonDownloader.java 	deleted:    MyDemos/src/com/example/mydemos/net/downloadqueue/Downloader.java 	deleted:    MyDemos/src/com/example/mydemos/net/downloadqueue/HttpComponentTaskDownloader.java 	deleted:    MyDemos/src/com/example/mydemos/net/downloadqueue/assist/statusstore/PerferenceStore.java 	deleted:    MyDemos/src/com/example/mydemos/net/downloadqueue/assist/statusstore/StatusStore.java 	deleted:    MyDemos/src/com/example/mydemos/net/netty/downloadqueue/DownLoadService.java 	deleted:    MyDemos/src/com/example/mydemos/net/netty/downloadqueue/Task.java 	modified:   readme

--- MyDemos/src/com/example/mydemos/net/downloadqueue/Downloader.java
+++
@@ -1,9 +0,0 @@
-package com.example.mydemos.net.downloadqueue;
-
-import com.example.mydemos.net.downloadqueue.bean.Task;
-
-
-public interface Downloader extends Runnable{
-	public void pause();
-	public void setTask(Task task);
-}
